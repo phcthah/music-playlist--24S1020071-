0f62947a0dd5e6d84870ec61cb57cf0f22bd8e19
phcthah <thanhne0805@gmail.com>
2025-12-02 08:10:47 +0700

    Tạo file playlist.py, khai báo biến toàn cục và viết hàm main

---
+++ playlist.py
@@ -0,0 +1,7 @@
+# Khai báo biến danh sách toàn cục
+songs = []
+def main():
+    print("Chào mừng đến với chương trình quản lý playlist!")
+    print("1. Hiển thị danh sách bài hát")
+    print("2. Thêm bài hát")
+    print("3. Thoát")
